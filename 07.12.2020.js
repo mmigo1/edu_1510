de8d5330d014e63f38c580f4b36b2a363464fdaa
mmigo1 <75628415+mmigo1@users.noreply.github.com>
2020-12-08 18:56:13 +0300

    Update 07.12.2020.js

--- 07.12.2020.js
+++ 07.12.2020.js
@@ -5,18 +5,32 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
-let words = ["Гобой","Флейта","Как звучит флейта","Балалайка","Фагот","Скрипка","Саксофон"];
+let sites = {
+   "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой","Флейта","Как звучит флейта","Балалайка","Фагот","Скрипка","Саксофон"],
+    "crushdrummers.ru":["Барабанное шоу", "Заказать барабанное шоу в Москве","Барабанщики на свадьбу", "Барабанщики на корпоротив"]
+}
+let site = Object.keys (sites)[getRandom(0,Object.keys(sites).length)];
+let words = sites[site];
 let word = words[getRandom(0,words.length)];
 let yandexInput = document.getElementsByName("text")[0];
 let button = document.getElementsByClassName("button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited")[0];
 if (button!=undefined){
     let i = 0;
-    let timerId = setInterval(function(){
+    document.cookie = "site="+site;
+    let timerId = setInterval(()=>{
         yandexInput.value = yandexInput.value + word [i];
         i++;
         if(i== word.length){
@@ -24,7 +38,8 @@ if (button!=undefined){
             button.click();
         }
     },550);
-}else{
+}else if (location.hostname == "yandex.ru"){
+    site = getCookie("site");
     let x = document.getElementsByClassName("pager__item pager__item_current_yes pager__item_kind_page")[0];
     let pageNum = x.textContent
     let pageButton = document.getElementsByClassName("pager__item_kind_next")[0];
@@ -32,7 +47,8 @@ if (button!=undefined){
     let links = document.links;
     for(let i=0; i<links.length; i++){
         let link = links[i]
-        if(link.href.includes("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")){
+        if(link.href.includes(site)){
+            link.removeAttribute('target');
             setTimeout(()=>{link.click();},1000);
             linkIsFound = true;
             break;
@@ -42,4 +58,14 @@ if (button!=undefined){
     }else if (!linkIsFound){
         location.href = "https://yandex.ru/";
     }
+}else{
+    if(getRandom(1,11) > 8 ) setTimeout(()=>{location.href = "https://yandex.ru/";},3000);
+    let links = document.links;
+    setInterval(()=>{
+        let index = getRandom(0,links.length);
+        let link = links[index];
+        if (link.href.includes(location.hostname)){
+            setTimeout(()=>{link.click();},3000);
+        }
+    },5000);
 }
